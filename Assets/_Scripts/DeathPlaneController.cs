4cf5276629bfbb25f250ded965312fe6913dd8ef
Ramandeep <rmnkaul979697@gmail.com>
2021-02-22 16:44:41 -0500

    added death plane and spawn point object

---
+++ Assets/_Scripts/DeathPlaneController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlaneController : MonoBehaviour
+{
+
+public Transform spawnPoint;
+
+  
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            var controller = other.gameObject.GetComponent<CharacterController>();
+            controller.enabled = false;
+            other.gameObject.transform.position = spawnPoint.position;
+            controller.enabled = true;
+        }
+    }
+}
